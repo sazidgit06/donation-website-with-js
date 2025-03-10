94ffb445ad94be4cd21a714379303f3bc2653b2f
Sazid <sazidtec06@gmail.com>
2025-03-10 23:16:45 +0600

    donation page and btn done history btn done

--- js/getClicked.js
+++ js/getClicked.js
@@ -1,7 +1,9 @@
 document.getElementById('donation-btn-id').addEventListener('click', function(){
     getColorById('donation-btn-id');
+    getInfo('donation-page-id');
 });
 
 document.getElementById('history-btn-id').addEventListener('click', function(){
     getColorById('history-btn-id');
-})
+    getInfo('history-page-id');
+});
--- js/reusuableFunction.js
+++ js/reusuableFunction.js
@@ -5,3 +5,10 @@ function getColorById(id){
     document.getElementById(id).classList.add('bg-[#B4F461]');
 
 }
+
+function getInfo(id){
+    document.getElementById('donation-page-id').classList.add('hidden');
+    document.getElementById('history-page-id').classList.add('hidden');
+
+    document.getElementById(id).classList.remove('hidden');
+}
